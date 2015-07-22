25cd9a826ec780afd5f95afe8bb534dddffd3dd9
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-07-22 22:35:39 +0900

    add operation collection.

--- JusshoWeb.js
+++ JusshoWeb.js
@@ -1,23 +1,36 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+Operations = new Mongo.Collection('operations');
+Operations.defaultName = function() {
+  var date = new Date();
+  for (var i = 1;; i++) {
+    var nextName = date.toISOString().split('T')[0] + 'の作戦' + i;
+    if(!Operations.findOne({name: nextName})) {
+      return nextName;
     }
+  }
+};
+
+if (Meteor.isClient) {
+
+  Template.operations.helpers({
+    operations: function () {
+      return Operations.find();
+    },
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.operations.events({
+    'click .new-operation': function(event) {
+      Operations.insert({
+        name: Operations.defaultName(),
+        //owner: Meteor.userId(),
+      })
+    },
+  });
+
+  Template.operation.events({
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
