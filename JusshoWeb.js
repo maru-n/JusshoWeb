fa85d9afdecd29d520744403b8bd3d2349ec076a
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-07-24 19:34:15 +0900

    implemented pictures collection.

--- JusshoWeb.js
+++ JusshoWeb.js
@@ -14,11 +14,28 @@ Operations.allow({
     return (userId && doc.owner === userId);
   },
   update: function (userId, doc, fields, modifier) {
-    return (userId && doc.owner === userId);
+    return _.contains(fields, "photos") || (userId && doc.owner === userId);
   },
   remove: function (userId, doc) {
     return (userId && doc.owner === userId);
   },
+});
+
+//FS.debug = true;
+
+var createSquareThumb = function(fileObj, readStream, writeStream) {
+    var size = '96';
+    gm(readStream).autoOrient().resize(size, size + '^').gravity('Center').extent(size, size).stream('PNG').pipe(writeStream);
+};
+
+Photos = new FS.Collection('photos', {
+    stores: [
+    new FS.Store.GridFS('originals', {}),
+    new FS.Store.GridFS("thumbs", { transformWrite: createSquareThumb })
+    ],
+    filter: {
+      allow: {contentTypes: ['image/*']}
+    }
 });
 
 Accounts.config({
@@ -31,6 +48,11 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  Template.home.helpers({
+    isOperationSelected: function() {
+      return Session.get("currentOperationId");
+    },
+  });
 
   Template.operationList.helpers({
     operations: function() {
@@ -59,6 +81,10 @@ if (Meteor.isClient) {
       })
       event.target.text.value = "";
     },
+    'click .operation-link': function(event) {
+      event.preventDefault();
+      Session.set("currentOperationId", this._id);
+    },
   });
 
   Template.operation.helpers({
@@ -76,6 +102,38 @@ if (Meteor.isClient) {
   Template.operation.events({
     'click .delete': function(event) {
       Operations.remove(this._id);
+    }
+  });
+
+  Template.photoList.helpers({
+    currentOperationName: function() {
+      var currentOperation = Operations.findOne(Session.get("currentOperationId"));
+      return currentOperation.name;
+    },
+    photos: function() {
+      var currentOperation = Operations.findOne(Session.get("currentOperationId"));
+      if (!currentOperation.photos) {
+        return;
+      };
+      var photos = Photos.find({
+        _id: {
+          $in: currentOperation.photos
+        }
+      });
+      return photos;
+    },
+  });
+
+  Template.photoList.events({
+    'change .upload-photos': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        var fileObj = Photos.insert(file);
+        Operations.update(
+          Session.get("currentOperationId"),
+          {
+            $addToSet: {photos: fileObj._id}
+          });
+      });
     }
   });
 }
