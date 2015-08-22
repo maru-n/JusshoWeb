cdfcb452dbbcd385de12bd678e01783d56e2a30f
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-08-22 18:15:59 +0900

    routing several url.

--- client/home.js
+++ client/home.js
@@ -2,17 +2,7 @@ Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
 });
 
-
 Template.home.helpers({
-    isOperationSelected: function() {
-        return Session.get("currentOperationId");
-    },
-    isUserUploading: function(){
-        var photos = Photos.find({
-            owner: Meteor.userId()
-        });
-        return photos;
-    },
 });
 
 Template.operationList.helpers({
@@ -26,6 +16,12 @@ Template.operationList.helpers({
     operationDefaultName: function() {
         return Operations.defaultName();
     },
+    ownerName: function() {
+        var owner = Meteor.users.findOne(this.owner);
+        if (owner) {
+            return owner.username;
+        };
+    }
 });
 
 Template.operationList.events({
@@ -35,71 +31,4 @@ Template.operationList.events({
         Meteor.call("createOperation", text);
         event.target.text.value = "";
     },
-    'click .operation-link': function(event) {
-        event.preventDefault();
-        Session.set("currentOperationId", this._id);
-    },
-});
-
-Template.operation.helpers({
-    isOwned: function() {
-        return this.owner === Meteor.userId();
-    },
-    ownerName: function() {
-        var owner = Meteor.users.findOne(this.owner);
-        if (owner) {
-            return owner.username;
-        };
-    }
-});
-
-Template.operation.events({
-    'click .delete': function(event) {
-        Meteor.call("deleteOperation", this._id);
-    }
-});
-
-Template.photoList.helpers({
-    currentOperationName: function() {
-        var currentOperation = Operations.findOne(Session.get("currentOperationId"));
-        return currentOperation.name;
-    },
-    currentOperationId: function() {
-        return Session.get("currentOperationId");
-    },
-    photos: function() {
-        var currentOperation = Operations.findOne(Session.get("currentOperationId"));
-        if (!currentOperation.photos) {
-            return null;
-        };
-        var photos = Photos.find({
-            _id: {
-                $in: currentOperation.photos
-            }
-        });
-        return photos;
-    },
-});
-
-Template.photoList.events({
-    'change .upload-photos': function(event) {
-        FS.Utility.eachFile(event, function(file) {
-            //Meteor.call("createPhoto", file, Session.get("currentOperationId"));
-            var operationId = Session.get("currentOperationId");
-            var newFile = new FS.File(file);
-            newFile.metadata = {
-                owner: Meteor.userId()
-            }
-            var fileObj = Photos.insert(newFile, function(){
-
-            });
-            Operations.update(operationId, {
-                $addToSet: {photos: fileObj._id}
-            });
-        });
-        event.target.value = null;
-    },
-    'click .delete-all-photos': function(event) {
-        Meteor.call("deleteAllPhotos", Session.get("currentOperationId"));
-    }
 });
