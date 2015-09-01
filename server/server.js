72672716418537335f7ad45fd903ff1393304309
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-09-02 02:37:58 +0900

    split functions about insert photo files on method of Photo collection.

--- server/server.js
+++ server/server.js
@@ -17,19 +17,3 @@ Meteor.startup(function () {
         });
     }
 });
-
-
-Slingshot.createDirective("photos", Slingshot.S3Storage, {
-    bucket: Meteor.settings.AWSS3Bucket,
-    acl: "public-read",
-    authorize: function (file, metaContext) {
-        if (!this.userId) {
-            var message = "Please login before posting files";
-            throw new Meteor.Error("Login Required", message);
-        }
-        return true;
-    },
-    key: function (file, metaContext) {
-        return "originals/" + metaContext.operationId + "/" + file.name;
-    }
-});
