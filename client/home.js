c509b46c361745299c797ba922ceac781db182e2
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-12-09 04:21:26 +0900

    add stylesheet.

--- client/home.js
+++ client/home.js
@@ -1,5 +1,6 @@
 Meteor.subscribe("operations");
 Meteor.subscribe("allUserName");
+Meteor.subscribe("coverPhotos");
 
 Template.operationList.helpers({
     operations: function() {
@@ -17,6 +18,15 @@ Template.operationList.helpers({
         if (owner) {
             return owner.username;
         };
+    },
+
+    coverPhotoUrl: function() {
+        var coverPhoto = Photos.findOne({operation:this._id});
+        try {
+            return coverPhoto.medium.url
+        } catch (e) {
+            return "http://placehold.jp/40/3f51b5/ffffff/400x300.png?text=%E5%86%99%E7%9C%9F%E3%81%8C%E3%81%AA%E3%81%84%EF%BC%81%EF%BC%81"
+        }
     },
 
     photoCount: function() {
